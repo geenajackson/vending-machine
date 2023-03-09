0806605eb1e92c1f4b443526dd99de9701f51cb0
Geena Jackson <geena.jackson96@gmail.com>
2023-03-09 12:38:30 -0600

    removed strict mode for links to work

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //removed strict mode to allow for link changes
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
